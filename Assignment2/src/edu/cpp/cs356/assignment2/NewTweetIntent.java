b3b80b4a552f22074c791c2367a90f556957a2ee
Xinyuan Wang <wxy03003wxy@hotmail.com>
2017-07-12 20:41:20 -0700

    clear

--- Assignment2/src/edu/cpp/cs356/assignment2/NewTweetIntent.java
+++
@@ -1,8 +0,0 @@
-package edu.cpp.cs356.assignment2;
-
-/**
- * Created by Xinyuan Wang on 7/9/2017.
- */
-public class NewTweetIntent implements Intent {
-
-}
--- Assignment2/src/edu/cpp/cs356/assignment2/Observer.java
+++
@@ -1,9 +0,0 @@
-package edu.cpp.cs356.assignment2;
-
-/**
- * Created by Xinyuan Wang on 7/9/2017.
- */
-public interface Observer {
-
-    public void update(Intent intent);
-}
